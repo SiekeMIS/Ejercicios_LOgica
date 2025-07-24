3256bcefd04c9bb0fadb8a36d5d638dfaa423d2d
SiekeMIS <lucasrosas.991402@gmail.com>
2025-07-24 18:02:49 -0400

    Ejercicio 5 POO

--- Ejercicios_POO.py
+++ Ejercicios_POO.py
@@ -1,6 +1,6 @@
 # """
 # Realizar un programa que conste de una clase llamada Estudiante, que tenga como atributos el nombre y la nota del alumno. Definir los métodos para 
-# inicializar sus atributos, imprimirlos y mostrar un mensaje con el resultado de la nota y si ha aprobado o no.
+# inicializar sus atributos, imprimirlos y mostrar un mensaje con el resultado de la nota y si ha aprobado o no. (1)
 # """
 # class Estudiante:
 # 	def __init__(self, nombre, nota):
@@ -24,7 +24,7 @@
 
 # """
 # Crea una clase “Persona”. Con atributos nombre y edad. Además, hay que crear un método “cumpleaños”, que aumente en 1 la edad de la persona cuando se invoque sobre un objeto creado con “Persona”.
-# Tendríamos que lograr ejecutar el siguiente código con la clase creada:
+# Tendríamos que lograr ejecutar el siguiente código con la clase creada: (2)
 # """
 
 # class Persona:
@@ -42,7 +42,7 @@
 
 # """
 # Realizar un programa en el cual se declaren dos valores enteros por teclado utilizando el método __init__. Calcular después la suma, resta, multiplicación y división. 
-# Utilizar un método para cada una e imprimir los resultados obtenidos. Llamar a la clase Calculadora.
+# Utilizar un método para cada una e imprimir los resultados obtenidos. Llamar a la clase Calculadora. (3)
 # """
 # class Calculadora:
 # 	def __init__(self, numero_uno, numero_dos):
@@ -77,3 +77,33 @@
 
 # calculo = Calculadora(10, 2)
 # calculo.división()
+
+
+# Crear una clase “Persona” que sea la clase padre de otra clase “Estudiante”. Por tanto:
+# En la clase “Persona” su método __init__() debe de estar preparado para recibir nombre y apellido. Además, esta clase , debe tener un método para mostrar nombre_completo() 
+# el cual debe mostrar el nombre acompañado del apellido.
+# La otra clase “Estudiante”, debe de poder heredar de “Persona”, y además recibir los argumentos nombre y edad. También la clase “Estudiante”, recibe el valor 
+# “carrera”, y además contar con un método mostrar_carrera(). Las dos clases son obligatorias. (4)
+
+# class Persona():
+#     def __init__(self, nombre, apellido):
+#         self.nombre = nombre
+#         self.apellido = apellido
+
+#     def nombre_apellido(self):  # Falta el parámetro self
+#         print(self.nombre + " " + self.apellido)  # Agregué espacio entre nombre y apellido
+
+# class Estudiente(Persona):  # Hay un typo en "Estudiente" (debería ser "Estudiante")
+#     def __init__(self, nombre, apellido, edad, carrera):  # Typo en "apellido" (debería ser "apellido")
+#         super().__init__(nombre, apellido)  # Typo en "apellido"
+#         self.edad = edad
+#         self.carrera = carrera
+
+#     def mostrar_carrera(self):  # Falta el parámetro self
+#         print(self.carrera)  # Debería mostrar self.carrera en lugar de redefinir la variable
+
+# joven_estudiante = Estudiente("Lucas", "Rosas", 20, "Psicologia")
+# print(joven_estudiante.nombre)
+# print(joven_estudiante.apellido)  # Typo en "apellido"
+# print(joven_estudiante.edad)
+# print(joven_estudiante.carrera)
