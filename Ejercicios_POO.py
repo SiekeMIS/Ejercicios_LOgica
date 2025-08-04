608b0864f8311a045db7cd02e26c9e77f0bcab42
SiekeMIS <lucasrosas.991402@gmail.com>
2025-08-04 17:45:25 -0400

    Ejercicio 6 POO

--- Ejercicios_POO.py
+++ Ejercicios_POO.py
@@ -136,3 +136,29 @@
 # print("Carro:")
 # carro = Carro(4, "Azul", "$4.500.990")
 # carro.mostrar_detalles()
+
+# """
+# Crear una clase llamada Marino(), con un método que sea hablar, en donde muestre un mensaje que diga «Hola, soy un animal marino!». Luego, crear una clase Pulpo() que herede Marino, pero modificar el mensaje de hablar por «Hola soy un Pulpo!».
+# Por último, crear una clase Foca(), heredada de Marino, pero que tenga un atributo nuevo llamado mensaje y que muestre ese mensaje como parámetro. (6)
+# """
+# class Marino():
+# 	def hablar(self):
+# 		print("Holis, soy un pececillo indefenso!")
+
+# class Pulpo(Marino):
+# 	def hablar(self):
+# 		print("Hola, soy un Pulpox!")
+
+# class Foca(Marino):
+# 	def hablar(self, mensaje):
+# 		self.mensaje = mensaje
+# 		print(mensaje)
+
+# marino = Marino()
+# marino.hablar()
+
+# pulpo = Pulpo()
+# pulpo.hablar()
+
+# foca = Foca()
+# foca.hablar("Hola, soy una foca!")
